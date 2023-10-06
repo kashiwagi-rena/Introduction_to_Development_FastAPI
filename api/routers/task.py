c5213e42d398755620fcff81d0b3324a0d1471aa
kashiwagi-rena <azisai07@gmail.com>
2023-10-06 15:32:08 +0900

    p.100 10-2 残りすべてのリクエストとレスポンスの定義

--- api/routers/task.py
+++ api/routers/task.py
@@ -12,10 +12,10 @@ async def list_tasks():
 async def create_task(task_body: task_schema.TaskCreate):
     return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
-@router.put("/tasks/{task_id}")
-async def update_task():
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
-@router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int):
+    return
