8232ee495b5d977b10843b72b31a011d4447430c
kashiwagi-rena <azisai07@gmail.com>
2023-10-06 10:21:03 +0900

    p.85 9-2 レスポンス型の定義

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
+import api.schemas.task as task_schema
+
 router = APIRouter()
 
-@router.get("/tasks")
+@router.get("/tasks", response_model=list[task_schema.Task])
 async def list_tasks():
-  pass
+  return [task_schema.Task(id=1, title="1つ目のToDoタスク")]
 
 @router.post("/tasks")
 async def create_task():
