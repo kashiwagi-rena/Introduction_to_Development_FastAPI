1e9efbe5947c2cc0cdc2443020fc8e0764b4bd56
kashiwagi-rena <azisai07@gmail.com>
2023-10-06 10:25:45 +0900

    p.77 8-2 ルーター

--- api/main.py
+++ api/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from api.routers import task, done
+
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+app.include_router(task.router)
+app.include_router(done.router)
