2b078d54e055f35a71b31200844655df397c5dfc
kashiwagi-rena <azisai07@gmail.com>
2023-10-06 11:41:39 +0900

    p.96 10-1 スキーマ・リクエスト

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel, Field
 
-
-class Task(BaseModel):
-  id: int
+class TaskBase(BaseModel):
   title: str | None = Field(None, example="クリーニングを取りに行く")
-  # title: bool | None = Field(None, example="クリーニングを取りに行く")
+
+class TaskCreate(TaskBase):
+  pass
+
+class TaskCreateResponse(TaskCreate):
+  id: int
+
+  class Config:
+    orm_mode = True
+
+class Task(TaskBase):
+  id: int
   done: bool = Field(False, description="完了フラグ")
+
+  class Config:
+    orm_mode = True
