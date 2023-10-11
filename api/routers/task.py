e62e2c223b5cdb038bf5a8b5da2f11f818dd7065
kashiwagi-rena <azisai07@gmail.com>
2023-10-11 21:47:07 +0900

    p.121 12-1 C: Create

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+import api.cruds.task as task_crud
+from api.db import get_db
 
 import api.schemas.task as task_schema
 
@@ -9,8 +13,8 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のToDoタスク")]
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate):
