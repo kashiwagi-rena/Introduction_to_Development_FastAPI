3e855ab1792d9cba61be2af2e117c9752f6c7e75
kashiwagi-rena <azisai07@gmail.com>
2023-10-13 07:52:50 +0900

    p.126 12-2 R:Read

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,3 +1,15 @@
+from sqlalchemy.orm import Session
+
+import api.models.task as task_model
+import api.schemas.task as task_schema
+
+def create_task(db:Session, task_create: task_schema.TaskCreate) -> task_model.Task:
+    task = task_model.Task(**task_create.dict())
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 
