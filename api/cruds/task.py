f516fe8629c3b78925cd37832045f829dfa7a062
kashiwagi-rena <azisai07@gmail.com>
2023-10-12 22:43:16 +0900

    p.124 12-2 R:Read

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,11 +1,13 @@
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
-import api.models.task as task_model
-import api.schemas.task as task_schema
+def get_tasks_with_done(db: Session) -> list[tuple[int, str, bool]]:
+    result: Result = db.execute(
+        select(
+            task_model.Task.id,
+            task_model.Task.title,
+            task_model.Done.id.isnot(None).label("done"),
+        ).outerjoin(task_model.Done)
+    )
 
-def create_task(db:Session, task_create: task_schema.TaskCreate) -> task_model.Task:
-    task = task_model.Task(**task_create.dict())
-    db.add(task)
-    db.commit()
-    db.refresh(task)
-    return task
+    return result.all()
