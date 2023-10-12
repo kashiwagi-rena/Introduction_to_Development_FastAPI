60a67dc79665fdd3863c89adad55ca05e5f9d4ae
kashiwagi-rena <azisai07@gmail.com>
2023-10-12 19:05:40 +0900

    誤打気をつけて！

--- api/db.py
+++ api/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "mysql+pymysql://root@db:3306/demo?charset=ytf8"
+DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
 
 db_engine = create_engine(DB_URL, echo=True)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
