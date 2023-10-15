06662d09fd9c8e56784f18708c71fc493130a76c
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 16:39:33 +0900

    p.149 13-4 非同期対応したCRUDs

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,8 +10,8 @@ import api.schemas.task as task_schema
 router = APIRouter()
 
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
