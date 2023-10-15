5e42a87a6de7671a777b09a6d2e58d8d82de08c4
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 16:17:01 +0900

    p.147 13-4 非同期対応したCRUDs

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
 from api.db import get_db
@@ -13,8 +14,8 @@ async def list_tasks(db: Session = Depends(get_db)):
     return task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
-    return task_crud.create_task(db, task_body)
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
