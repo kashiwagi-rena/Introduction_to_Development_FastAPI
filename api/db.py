f66bf57889b7a854a6522f7528bb46317fa79328
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 15:56:52 +0900

    p.144 13-3 非同期対応したDB接続関数

--- api/db.py
+++ api/db.py
@@ -1,13 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
+ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
-db_engine = create_engine(DB_URL, echo=True)
-db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
 
 Base = declarative_base()
 
-def get_db():
-  with db_session() as session:
+async def get_db():
+  async with async_session() as session:
     yield session
