9f15e8f71a095c40cd7aa3356bc085a31db4fb7a
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 16:48:46 +0900

    p.151 13-4 非同期対応したCRUDs

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
@@ -26,9 +25,9 @@ async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Async
     return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = task_crud.get_task(db, task_id=task_id)
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     
-    return task_crud.delete_task(db, original=task)
+    return await task_crud.delete_task(db, original=task)
