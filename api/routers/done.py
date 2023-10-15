db643a9a48c8373f88e007f8289db15eb87e64e5
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 13:01:54 +0900

    p.135 12-5 Doneリソース

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+import api.schemas.done as done_schema
+import api.cruds.done as done_crud
+from api.db import get_db
 
 router = APIRouter()
 
-@router.put("/tasks/{task_id}/done", response_model=None)
-async def mark_task_as_done(task_id: int):
-  return
+@router.put("/tasks/{task_id}/done", response_model=done_schema.DoneResponse)
+async def mark_task_as_done(task_id: int, db: Session = Depends(get_db)):
+  done = done_crud.get_done(db, task_id=task_id)
+  if done is not None:
+    raise HTTPException(status_code=400, detail="Done already exists")
+  
+  return done_crud.create_done(db, task_id)
 
 @router.delete("/tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_id: int):
-  return
+async def unmark_task_as_done(task_id: int, db: Session = Depends(get_db)):
+  done = done_crud.get_done(db, task_id=task_id)
+  if done is None:
+    raise HTTPException(status_code=404, detail="Done not found")
+  
+  return done_crud.delete_done(db, original=done)
