454be714ba452b4d0778c5108fc9fb30cc82c3b4
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 17:06:40 +0900

    p.153 13-4 非同期対応したCRUDs

--- api/cruds/done.py
+++ api/cruds/done.py
@@ -1,22 +1,22 @@
 from sqlalchemy  import select
 from sqlalchemy.engine import Result
-from sqlalchemy.orm import Session
+from sqlalchemy.orm.asyncio import AsyncSession
 
 import api.models.task as task_model
 
-def get_done(db: Session, task_id: int) -> task_model.Done | None:
-  result: Result = db.execute(
+async def get_done(db: AsyncSession, task_id: int) -> task_model.Done | None:
+  result: Result = await db.execute(
     select(task_model.Done).filter(task_model.Done.id == task_id)
   )
   return result.scalars().first()
 
-def create_done(db: Session, task_id: int) -> task_model.Done:
+async def create_done(db: AsyncSession, task_id: int) -> task_model.Done:
   done = task_model.Done(id=task_id)
   db.add(done)
-  db.commit()
-  db.refresh(done)
+  await db.commit()
+  await db.refresh(done)
   return done
 
-def delete_done(db: Session, original: task_model.Done) -> None:
-  db.delete(original)
-  db.commit()
+async def delete_done(db: AsyncSession, original: task_model.Done) -> None:
+  await db.delete(original)
+  await db.commit()
