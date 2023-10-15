6f5bd465936d691379572ab716a288285ad4f604
kashiwagi-rena <azisai07@gmail.com>
2023-10-15 17:06:48 +0900

    p.153 13-4 非同期対応したCRUDs

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.schemas.done as done_schema
 import api.cruds.done as done_crud
@@ -8,17 +8,17 @@ from api.db import get_db
 router = APIRouter()
 
 @router.put("/tasks/{task_id}/done", response_model=done_schema.DoneResponse)
-async def mark_task_as_done(task_id: int, db: Session = Depends(get_db)):
-  done = done_crud.get_done(db, task_id=task_id)
+async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
+  done = await done_crud.get_done(db, task_id=task_id)
   if done is not None:
     raise HTTPException(status_code=400, detail="Done already exists")
   
-  return done_crud.create_done(db, task_id)
+  return await done_crud.create_done(db, task_id)
 
 @router.delete("/tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_id: int, db: Session = Depends(get_db)):
-  done = done_crud.get_done(db, task_id=task_id)
+async def unmark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
+  done = await done_crud.get_done(db, task_id=task_id)
   if done is None:
     raise HTTPException(status_code=404, detail="Done not found")
   
-  return done_crud.delete_done(db, original=done)
+  return await done_crud.delete_done(db, original=done)
