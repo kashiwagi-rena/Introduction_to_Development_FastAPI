7d517528bcc3c9342beb09160515baebb70dc71c
kashiwagi-rena <azisai07@gmail.com>
2023-10-20 16:51:37 +0900

    p.161 14-3 テストを書く(1)

--- tests/test_main.py
+++ tests/test_main.py
@@ -31,3 +31,19 @@ app.dependency_overrides[get_db] = get_test_db
 # テスト用に非同期HTTPクライアントを返却
 async with AsyncClient(app=app, base_url="http://test") as client:
   yield client
+
+import starlette.status
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+  response = await async_client.post("/tasks", json={"title": "テストタスク"})
+  assert response.status_code == starlette.status.HTTP_200_OK
+  response_obj = response.json()
+  assert response_obj["title"] == "テストタスク"
+
+  response = await async_client.get("/tasks")
+  assert response.status_code == starlette.status.HTTP_200_OK
+  response_obj = response.json()
+  assert len(response_obj) == 1
+  assert response_obj[0]["title"] == "テストタスク"
+  assert response_obj[0]["done"] is False
