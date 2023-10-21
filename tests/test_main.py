1e616d956899fae235c897ce434218eab77d46bf
kashiwagi-rena <azisai07@gmail.com>
2023-10-21 23:18:45 +0900

    p.162 14-4 テストを書く(2)

--- tests/test_main.py
+++ tests/test_main.py
@@ -47,3 +47,26 @@ async def test_create_and_read(async_client):
   assert len(response_obj) == 1
   assert response_obj[0]["title"] == "テストタスク"
   assert response_obj[0]["done"] is False
+
+@pytest.mark.asyncio
+async def test_done_flag(async_client):
+  response = await async_client.post("/tasks", json={"title": "テストタスク2"})
+  assert response.status_code == starlette.status.HTTP_200_OK
+  response_obj = response.json()
+  assert response_obj["title"] == "テストタスク2"
+
+  # 完了フラグを立てる
+  response = await async_client.put("/tasks/1/done")
+  assert response.status_code == starlette.status.HTTP_200_OK
+
+  # 既に完了フラグが立っているので400を返却
+  response = await async_client.put("/tasks/1/done")
+  assert response.status_code == starlette.status.HTTP_400_BAD_REQUEST
+
+  # 完了フラグを外す
+  response = await async_client.delete("/tasks/1/done")
+  assert response.status_code == starlette.status.HTTP_200_OK
+
+  # 既に完了フラグが外れているので404を返却
+  response = await async_client.delete("/tasks/1/done")
+  assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
