7792f65898e0c4e9c526f7cd552ef4f1cb5b749a
kashiwagi-rena <azisai07@gmail.com>
2023-10-23 22:09:43 +0900

    p.171 14-6 parametrizeテスト

--- tests/test_main.py
+++ tests/test_main.py
@@ -84,3 +84,18 @@ async def test_due_date(async_client):
 
   response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": "2024-1201"})
   assert response.status_code == starlette.status.HTTP_422_UNPROCESSABLE_ENTIT
+
+@pytest.mark.asyncio
+async def test_due_date(async_client):
+  input_list = ["2024-12-01", "2014-12-32", "2024/12/01", "2024-1201"]
+  response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": input_param})
+  assert response.status_code == starlette.status.HTTP_200_OK
+
+  response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": input_param})
+  assert response.status_code == starlette.status.HTTP_422_UNPROCESSABLE_ENTITY
+
+  response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": input_param})
+  assert response.status_code == starlette.status.HTTP_422_UNPROCESSABLE_ENTITY
+
+  response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": input_param})
+  assert response.status_code == starlette.status.HTTP_422_UNPROCESSABLE_ENTITY
